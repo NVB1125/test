b040f899356ab639e8aa028baa43ca8e904ac3b7
CuteMonkey-art <72667793+CuteMonkey-art@users.noreply.github.com>
2020-11-05 06:29:47 +0900

    add_google

--- angular/src/app/main/pages/authentication/login/login.component.ts
+++ angular/src/app/main/pages/authentication/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation, ViewChild ,ElementRef} from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 import { FuseConfigService } from "@fuse/services/config.service";
@@ -17,7 +17,11 @@ import { MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBar
   encapsulation: ViewEncapsulation.None,
   animations: fuseAnimations,
 })
+
+
 export class LoginComponent implements OnInit {
+  auth2: any;
+  @ViewChild('ingoogle', {static: true }) img: ElementRef;
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
   loginForm: FormGroup;
@@ -57,19 +61,56 @@ export class LoginComponent implements OnInit {
     };
   }
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Lifecycle hooks
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * On init
-   */
   ngOnInit(): void {
     this.loginForm = this._formBuilder.group({
       email: ["", [Validators.required, Validators.email]],
       password: ["", Validators.required],
     });
+    this.googleSDK();
   }
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  //
+  prepareLoginImg() { 
+    this.auth2.attachClickHandler(this.img.nativeElement, {},
+      (googleUser) => {   
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        //YOUR CODE HERE
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+   
+  }   
+  googleSDK() { 
+    window['googleSDKLoaded'] = () => {
+      window['gapi'].load('auth2', () => {
+        this.auth2 = window['gapi'].auth2.init({
+          client_id: '188637199174-dnm6dm1r7k652d8ddqd122kgas9kho3e.apps.googleusercontent.com',
+          cookiepolicy: 'single_host_origin',
+          scope: 'profile email'
+        });
+        this.prepareLoginImg();
+      });
+    }
+   
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'google-jssdk'));
+   
+  }
+  /**
+   * On init
+   */ 
 
   loginBtn(): void {
     this.process = 'sending';
